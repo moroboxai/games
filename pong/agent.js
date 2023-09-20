85b6ac19d0630a8aa372eb18ebfb57844bbe5cc8
Nauja <jeremymorosi@hotmail.fr>
2023-09-20 13:30:24 +0200

    Create agent.js

---
+++ pong/agent.js
@@ -0,0 +1,27 @@
+/**
+ * The state of the game is in the form:
+ * {
+ *   ball: {x: number, y: number},
+ *   bars: [
+ *     {x: number, y: number},
+ *     {x: number, y: number}
+ *   ]
+ * }
+ * 
+ * This function must return inputs done by
+ * the agent for current frame in the form:
+ * {
+ *   left?: boolean,
+ *   right?: boolean,
+ *   up?: boolean,
+ *   down?: boolean
+ * }
+ * 
+ * Happy hacking :)
+ */
+function inputs(state) {
+    return {
+        up: state.ball.y < state.bars[0].y,
+        down: state.ball.y > state.bars[0].y,
+    };
+}
