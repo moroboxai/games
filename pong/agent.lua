15e7b046875b6f7ebbeeba67c71dd53c1db3c6db
Nauja <jeremymorosi@hotmail.fr>
2023-09-21 18:47:01 +0200

    Update agent.lua

--- pong/agent.lua
+++ pong/agent.lua
@@ -1,24 +1,22 @@
-/**
- * The state of the game is in the form:
- * {
- *   ball: {x: number, y: number},
- *   bars: [
- *     {x: number, y: number},
- *     {x: number, y: number}
- *   ]
- * }
- * 
- * This function must return inputs done by
- * the agent for current frame in the form:
- * {
- *   left?: boolean,
- *   right?: boolean,
- *   up?: boolean,
- *   down?: boolean
- * }
- * 
- * Happy hacking :)
- */
+-- The state of the game is in the form:
+-- {
+--   ball: {x: number, y: number},
+--   bars: [
+--     {x: number, y: number},
+--     {x: number, y: number}
+--   ]
+-- }
+-- 
+-- This function must return inputs done by
+-- the agent for current frame in the form:
+-- {
+--   left?: boolean,
+--   right?: boolean,
+--   up?: boolean,
+--   down?: boolean
+-- }
+-- 
+-- Happy hacking :)
 function inputs(state)
     d = {}
     d["up"] = state.ball.y < state.bars[0].y
