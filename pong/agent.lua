46e751937daeba601a4458bb1d8dd3a92543dd9b
Nauja <jeremymorosi@hotmail.fr>
2023-09-22 21:06:28 +0200

    Update agent.lua

--- pong/agent.lua
+++ pong/agent.lua
@@ -19,6 +19,7 @@
 -- Happy hacking :)
 function inputs(state)
     d = {}
-    d["up"] = state.ball.y < state.bars[0].y
-    d["down"] = state.ball.y > state.bars[0].y
+    d["up"] = state["ball"]["y"] < state["bars"][1]["y"]
+    d["down"] = state["ball"]["y"] > state["bars"][1]["y"]
+    return d
 end
